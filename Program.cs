47fda16d46789db9dd6e575d10661ad89280bf3f
vurvura1884 <97406507+vurvura1884@users.noreply.github.com>
2022-01-09 23:16:52 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        //Singleton
+        public class Rector
+        {
+            static Rector rector;
+            protected Rector()
+            {
+                Console.WriteLine("Назначен новый ректор");
+            }
+            public static Rector Instance()
+            {
+                if (rector == null)
+                {
+                    rector = new Rector();
+                }
+                else
+                {
+                    Console.WriteLine("Ректор уже есть");
+                }
+                return rector;
+            }
+        }
+        static void Main(string[] args)
+        {
+            //Singleton
+            Rector r_1 = Rector.Instance();
+            Rector r_2 = Rector.Instance();
+        }
+    }
+}
